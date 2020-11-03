7008c986473d8fe45134ad526148c0285a9e5ec9
dpandya <dpandya@vitahealthsoftware.com>
2020-11-03 16:05:52 +0530

    Made Routes Dynamic Based upon Features.

--- src/utility/Routers.js
+++ src/utility/Routers.js
@@ -2,24 +2,36 @@
 import React,{Suspense} from 'react';
 import { Route, Router, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import {CurrentContext } from '../utility/AppContext';
 import BeforeLoginContainer from '../ContainerPages/BeforeLoginContainer.js';
-const Dashboard = React.lazy(()=> import('../DashboardPages/Dashboard.js'));
-const LoginPage = React.lazy(()=> import('../LoginPages/LoginPage.js'));
-const ForgotPassword = React.lazy(()=> import('../LoginPages/ForgotPassword.js'));
-const RegisterUser = React.lazy(()=> import('../Users/RegisterUser.js'));
+
+const Components = {
+    "Register": () =>{return React.lazy(()=> import('../Users/RegisterUser.js'));},
+    "ForgotPassword": () =>{return React.lazy(()=> import('../LoginPages/ForgotPassword.js'));},
+    "Login": () =>{return React.lazy(()=> import('../LoginPages/LoginPage.js'));},
+}
 
 const history = createBrowserHistory();
 
 function Routers(props) {
+    const GetFeatures = () =>{
+        let currentContext = CurrentContext();
+        if(currentContext.MasterData.User){
+            return currentContext.MasterData.Features.find(e => e.loginrequired === true);
+        }
+        return currentContext.MasterData.Features;
+    }
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Router history={history}>
                 <Switch>
-                    <Route exact path={"/"} render={() => {return Suspension(LoginPage)}} />
-                    <Route exact path={"/register"} render={()=>{return Suspension(RegisterUser)}} />
-                    <Route exact path={"/login"} render={() => {return Suspension(LoginPage);}} />
-                    <Route exact path={"/forgotpassword"} render={() => {return Suspension(ForgotPassword);}} />
-                    <Route exact path={"/dashboard"} render={() => {return Suspension(Dashboard);}} />
+                    <Route exact path={"/"} render={() => {return Suspension(Components["Login"]())}} />
+                    {
+                        GetFeatures().map(i =>{
+                            return <Route key={i.feature} exact path={`/${i.target}`} render={() => {return Suspension(Components[i.feature]());}} />
+                        })
+                    }
                 </Switch>       
             </Router>
         </Suspense>
